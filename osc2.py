9ba5b276a1db02e190d8007be9b90755b3048f50
Robert Woodley <blwoodley@yahoo.com>
2019-03-02 17:54:42 -0600

    initial commit

---
+++ osc2.py
@@ -0,0 +1,53 @@
+import json
+import requests
+
+
+class Osc2:
+    # Use Version 2 of OpenSphericalCamera API to get a live video stream.
+    # https://developers.google.com/streetview/open-spherical-camera/
+
+    def __init__(self, ip_base="192.168.1.1", http_port=80):
+        self.response = None
+        self._ip = ip_base
+        self._http_port = http_port
+
+    def get_live_preview(self):
+        acquired = False
+
+        url = self.get_url("commands/execute")
+        body = json.dumps({"name": "camera.getLivePreview"})
+
+        try:
+            response = requests.post(url, data=body, headers={'Content-Type': 'application/json'}, stream=True)
+        except Exception as e:
+            print("HTTP Error: {}".format(repr(e)))
+            return acquired
+
+        if response.status_code == 200:
+            self.response = response
+            return True
+        else:
+            osc_error(response)
+            return False
+
+    def get_url(self, url_request):
+        osc_request = str("/osc/" + url_request)
+        url_base = "http://%s:%s" % (self._ip, self._http_port)
+        url = url_base + osc_request
+        return url
+
+
+def osc_error(request):
+    status = request.status_code
+
+    try:
+        error = request.json()
+
+        print("OSC Error - HTTP Status : %s" % status)
+        if 'error' in error:
+            print("OSC Error - Code        : %s" % error['error']['code'])
+            print("OSC Error - Message     : %s" % error['error']['message'])
+        print("OSC Error - Name        : %s" % error['name'])
+        print("OSC Error - State       : %s" % error['state'])
+    except:
+        print("OSC Error - HTTP Status : %s" % status)
---
+++ server.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template, Response
+from osc2 import Osc2
+import logging
+# see line 398 of connectionpool.py:
+logging.basicConfig(level=logging.DEBUG)
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # Video streaming home page.
+    return render_template('index.html')
+
+
+def gen(thetav):
+    bytes = ''
+    a = -1
+    # Video streaming generator function.
+    # Handles MJPEG stream.
+    for block in thetav.response.iter_content(16384):
+        print("Read Block ")
+        if bytes == '':
+            bytes = block
+        else:
+            bytes += block
+
+        # Search the current block of bytes for the jpq start and end
+        if a == -1:
+            a = bytes.find(b'\xff\xd8')
+        b = bytes.find(b'\xff\xd9')
+
+        if a != - 1 and b != -1:
+            print("Writing frame %04d - Byte range : %d to %d" % (0, a, b))
+            # Found a jpg, write to disk
+            frame = bytes[a:b + 2]
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+            # Reset the buffer to point to the next set of bytes
+            bytes = bytes[b + 2:]
+            print("Wrote frame.")
+            a = -1
+
+
+
+@app.route('/video_feed')
+def video_feed():
+    thetav = Osc2()
+    thetav.get_live_preview()
+    return Response(gen(thetav),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', threaded=True)
